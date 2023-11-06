4fb2088ee407e9e62796bfee87c2b9aa6bd38b2b
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-11-06 15:29:29 -0500

    📃 docs: add DOC

--- Iterable.js
+++ Iterable.js
@@ -1,9 +1,11 @@
+//NumberRange is a iterator of numbers from the range [i...j]
 export class NumberRange {
   constructor(firstNumber, lastNumber) {
     this.firstNumber = firstNumber;
     this.lastNumber = lastNumber;
     this.currentValue = firstNumber;
   }
+  // implement a iterator
   next() {
     if (this.currentValue <= this.lastNumber) {
       return { value: this.currentValue++, done: false };
